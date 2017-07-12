9fbe8240efba389e2346e770b7e79ec48d3e5d0e
王皓23182 <wh23182@ly.com>
2017-07-12 17:40:59 +0800

    JS优化

--- g1/g1.js
+++ g1/g1.js
@@ -1,14 +1,44 @@
-///http://code.jquery.com/jquery-latest.js
-var canvas = $("#canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
-document.body.appendChild(canvas);
+///<reference path="http://code.jquery.com/jquery-latest.js" />
+//http://www.cnblogs.com/leestar54/p/6411495.html
+; (function (window, jQuery, undefined) {
+    var config = {};
+    
+    var w = window;
+	requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+	
+    //页面参数
+    var pageInfo = {
+        topMenuIndex: "",
+        leftMenuIndex: "",
+        minMenuIndex: "",
+        pageType: false,
+    };
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
-};
-heroImage.src = "images/hero.png";
+   
+
+    var page = {
+        //初始化
+        Init: function (inConfig) {
+            config = inConfig;
+            basePath = config.basePath;
+            page.InitData();
+            page.InitEvents();
+        },
+        //初始化事件
+        InitEvents: function () {
+           
+        },
+        //初始化数据
+        InitData: function () {
+           
+        }
+    };
+
+    //事件类
+    var events = {
+       
+    }
+
+    //全局变量，请勿重新定义（多个JS文件需要区分）
+    window.g1PageInit = page.Init;
+})(window, jQuery)
